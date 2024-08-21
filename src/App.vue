bd06aef25529b41b29d0ecdfbb2c15d4c51c91fb
NmeoHuang1104 <q113113113q@gmail.com>
2024-08-21 20:15:11 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import nemo from './views/nemo.vue';
 </script>
 
 <template>
   <header>
+    <nemo></nemo>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
