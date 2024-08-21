8317e33bb3e7c94561578f3df880a5fb42773eba
wennie <wennieshe1235@gmail.com>
2024-08-21 20:40:57 +0800

    ADD ROUTER

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import Wennie from './views/Wennie.vue';
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/wennie">Wennie</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/wennie',
+      name: 'wennie',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Wennie.vue')
     }
   ]
 })
