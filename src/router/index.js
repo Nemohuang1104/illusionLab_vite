5c24dbb54608b64b4732f7d2fff69c9dec933ae5
NmeoHuang1104 <q113113113q@gmail.com>
2024-08-22 19:18:55 +0800

    new add SASS and vite

--- src/router/index.js
+++ src/router/index.js
@@ -5,45 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+     
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-<<<<<<< HEAD
-      path: '/lifeCasino',
-      name: 'lifeCasino',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/lifeCasino.vue')
-    },
-    {
-      path: '/wennie',
-      name: 'wennie',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/Wennie.vue')
-=======
-      path: '/nemo',
-      name: 'nemo',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/nemo.vue')
->>>>>>> nemo
-    }
 
-  ]
+    
+    ]
 })
 
 export default router
