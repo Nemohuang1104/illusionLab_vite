7afe56858287496f0ea3ac0dff38d56c07f28454
Ting Yuuuu <wendy97413@gmail.com>
2024-08-27 11:07:21 +0800

    新增sass顏色的部分~ 會員登入、會員註冊

--- src/preview-ting.js
+++ src/preview-ting.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
-import LittleQuiz from './views/LittleQuiz.vue'
+import LittleQuiz from './views/LittleQuiz.vue';
+import MindSpectrum from'./views/MindSpectrum-index.vue';
+import Login from './views/Login.vue';
+import SignUp from './views/SignUp.vue'
 
-createApp(LittleQuiz).mount('#preview')
+createApp(SignUp).mount('#preview');
--- src/views/MindSpectrum-index.vue
+++ src/views/MindSpectrum-index.vue
@@ -5,12 +5,16 @@
 
 <template>
   <div>
-
+    <h1>測試測試</h1>
   </div>
 </template>
 
 
 
 <style lang="scss" scoped>
+  @import "../assets/SASS/basic/color";
 
+  h1{
+    color: map-get($colofont_3, orange);
+  }
 </style>
