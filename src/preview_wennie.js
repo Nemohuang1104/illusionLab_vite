b7830e7d408d1bc193c59aeecf79d1594bb07c52
wennie <wennieshe1235@gmail.com>
2024-08-31 17:51:29 +0800

    20240831耶穌超愛我again

--- src/preview_wennie.js
+++ src/preview_wennie.js
@@ -10,18 +10,25 @@ import { createApp } from 'vue';
 // import MemberCMS from './views/MemberCMS.vue';
 // import CenterCMS from './views/CenterCMS.vue';
 // import MS_com_ticket_intro from './components/MS/MS_com_ticket_intro.vue';
-import MindSpectrum from './views/MindSpectrum.vue';
+// import MindSpectrum from './views/MindSpectrum.vue';
+// import SignUp from './views/SignUp.vue';
+// import Header_0 from './components/Header_0.vue';
+
+// import OrderProductPopCMS from './components/OrderProductPopCMS.vue';
+import OrderTicketPopCMS from './components/OrderTicketPopCMS.vue';
 
 /*使用路由preview*/
+
+//1.後臺管理路由========================
 // import previewrouter from "./router/preview_wennie_index";
 // const app = createApp(CenterCMS);
 // app.use(previewrouter).mount('#preview-app');
 
-
+//2.會員中心路由========================
 // import previewrouter from "./router/preview_wennie_index";
 // const app = createApp(MemberCenter);
 // app.use(previewrouter).mount('#preview-app');
  
 /*一般頁面preview*/
-const app = createApp(MindSpectrum);
+const app = createApp(OrderTicketPopCMS);
 app.mount('#preview-app');
